2efc69af6d372f33a6c109007ddb5ca389ebe85e
Muluken Menberu <muluken.menberu@idp.com>
2023-12-25 08:42:13 +0000

    JWT decode and encode added

--- srv/login-service.js
+++ srv/login-service.js
@@ -1,4 +1,23 @@
 const cds = require("@sap/cds");
+const jwt = require('jsonwebtoken');
+const secretKey = 'testTokenSecret'; 
+
+
+const generateJWT = (userData) => {
+    const token = jwt.sign(userData, secretKey, { expiresIn: '1h' }); 
+    return token;
+  };
+  
+  // Function to decode JWT
+  const decodeJWT = (token) => {
+    try {
+      const decoded = jwt.verify(token, secretKey);
+      return decoded;
+    } catch (error) {
+      return null; // Token is invalid or expired
+    }
+  };
+
 
 module.exports = cds.service.impl(async function (srv) {
     const db = await cds.connect.to('db');
@@ -12,16 +31,21 @@ module.exports = cds.service.impl(async function (srv) {
         const user = await db.read(Users).where({ Email, Password });
 
         if (user.length > 0) {
-            // User found, return user information
+            const token = generateJWT(user[0]);
+            user[0].token = token
+            
             return { user: user[0] };
         } else {
-            // User not found, return 'user not found' JSON
             return { message: 'User not found' };
         }
 
-        // fetch data from users based on this username and password if existed return user information if not return usernot found json
         return req.data;
     })
 
 
 });
+
+/*
+    const token = req.headers.authorization.split(' ')[1]; // Assuming the token is sent in the Authorization header
+    const decodedData = decodeJWT(token);
+    */
