ba71dc3ae218242a7ded0de04ba151c1bb048e7f
Muluken Menberu <muluken.menberu@idp.com>
2024-01-23 14:50:54 +0000

    ..

--- app/project1/webapp/config/Config.js
+++ app/project1/webapp/config/Config.js
@@ -1,7 +1,7 @@
 sap.ui.define([], function () {
     "use strict";
-
+ 
     return {
-        baseUrl: "https://port4004-workspaces-ws-wml98.us10.trial.applicationstudio.cloud.sap/",
+        baseUrl: "http://localhost:4004/",
     };
 });
