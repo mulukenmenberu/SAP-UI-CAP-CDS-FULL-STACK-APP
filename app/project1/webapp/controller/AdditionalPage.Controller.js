443238c878e15ef04f2f473dcc2cfcd5888a9561
Muluken Menberu <muluken.menberu@idp.com>
2024-01-23 14:53:13 +0000

    additionakl pavge controller

---
+++ app/project1/webapp/controller/AdditionalPage.Controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+  ], function (Controller) {
+    "use strict";
+  
+    return Controller.extend("project1.controller.AdditionalPage", {
+      // Your controller logic for SubItem1 goes here
+      
+      onButtonClick: function () {
+      alert('you clicked me man')
+		},
+   
+     
+    });
+  });
