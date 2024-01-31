5c592d71c50d8803cc7bb582eaee86ba379ca6d6
Muluken Menberu <mulerapps@gmail.com>
2024-01-31 14:23:16 +0300

    crp review

--- app/project1/webapp/config/Config.js
+++ app/project1/webapp/config/Config.js
@@ -2,6 +2,6 @@ sap.ui.define([], function () {
     "use strict";
 
     return {
-        baseUrl: "https://port4004-workspaces-ws-wml98.us10.trial.applicationstudio.cloud.sap/",
+        baseUrl: "http://localhost:4004/",
     };
 });
